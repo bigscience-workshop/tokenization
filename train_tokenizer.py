9fcf1bfdb95ee8849486ab2e5a1b354d7a8d5270
Samson Tan <samsontmr@users.noreply.github.com>
2022-01-10 15:53:05 +0800

    Update train_tokenizer.py

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -37,15 +37,12 @@ if args.data_files:
     oscar_datasets = load_dataset('text', data_files=paths).shuffle(seed=108)['train']
 elif args.parts >= 1:
     parts = [i for i in range(1, int(args.parts)+1)]
-    oscar_datasets = {lg: load_dataset('text', 
-                                       data_files=[construct_file_path(args.data_base_folder, lg, i) 
-                                                   for i in parts]).shuffle(seed=108)['train'] 
-                      for lg in lgs}
+    oscar_datasets = load_dataset('text', data_files=[construct_file_path(args.data_base_folder, lg, i) 
+                                  for i in parts for lg in lgs]).shuffle(seed=108)['train']
 else:
-    oscar_datasets = {lg: load_dataset('text', 
-                                       data_files=construct_file_path(args.data_base_folder, lg, args.parts))
-                          .shuffle(seed=108)['train'] 
-                      for lg in lgs}
+    oscar_datasets = load_dataset('text', data_files=[construct_file_path(args.data_base_folder, lg, args.parts) for lg in lgs])\
+                     .shuffle(seed=108)['train']
+
 
 def dataset_iterator(dataset):
     for i in range(len(dataset)):
@@ -69,14 +66,9 @@ make_folder('timings')
 for segmenter in args.segmenters.split(','):
     timing_file = 'timings/timings_per_lg_'+segmenter+'_'+str(args.parts)+'_oscar_vocab_'+str(args.vocab_size)+'.json'
     timings = json.load(open(timing_file, 'r')) if os.path.exists(timing_file) else {}
-    if args.data_files:
-        lg = "-".join(lgs)
-        time_taken = train_tokenizer(oscar_datasets, segmenter, lg, args.vocab_size, os.path.join(args.output_folder, lg))
-        timings[lg] = time_taken
-    else:
-        for lg in lgs:
-            print(segmenter, lg)
-            time_taken = train_tokenizer(oscar_datasets[lg], segmenter, lg, args.vocab_size, os.path.join(args.output_folder, lg))
-            timings[lg] = time_taken
+    lg = "-".join(lgs)
+    time_taken = train_tokenizer(oscar_datasets, segmenter, lg, args.vocab_size, os.path.join(args.output_folder, lg))
+    timings[lg] = time_taken
+   
 
     json.dump(timings, open('timings/timings_per_lg_'+segmenter+'_'+str(args.parts)+'_oscar_vocab_'+str(args.vocab_size)+'.json', 'w'), indent=4)
