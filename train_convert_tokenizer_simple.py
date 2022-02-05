80d1aca8cba39bec5e6741c80d0022d3a0da51d8
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-02-05 23:56:40 +0100

    unknown has to be specified

--- train_convert_tokenizer_simple.py
+++ train_convert_tokenizer_simple.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import sentencepiece as spm
-from datasets import load_dataset
+from datasets import load_dataset, utils
 from transformers.convert_slow_tokenizer import SpmConverter
 import argparse, os
 
@@ -11,11 +11,27 @@ def get_args():
     parser.add_argument("--data_name", "-d", type=str, required=True)
     parser.add_argument("--output_folder", "-o", type=Path, required=True)
     parser.add_argument("--num_threads", "-th", type=int, required=True)
+    parser.add_argument("--batch_size", type=int, default=1)
     return parser.parse_args()
 
-def dataset_iterator(dataset):
+def dataset_iterator(dataset, batch_size: int):
+    # WIP
+    # slices = [(start, min(len(datasets), start + batch_size)) for start in range(0, len(dataset), batch_size)]
+    # for i in utils.tqdm(
+    #     ,
+    #     total=len(dataset),
+    #     unit="ba",
+    #     disable=bool(utils.logging.get_verbosity() == utils.logging.NOTSET),
+    #     desc="Loading dataset",
+    # ):
+    #     # Load things by batch.
+    #     batch = dataset[i: i+batch_size]["text"]
+    #     for text in batch:
+    #         yield text
+    #
     for i in range(len(dataset)):
-        yield dataset[i]["text"] # assume relevant data is stored in 'text' field (datasets convention)
+        yield dataset[i]["text"]
+
 
 class SPMTokenizer:
     def __init__(self, vocab_file):
@@ -29,16 +45,16 @@ def main():
     dataset = load_dataset(args.data_name, data_files="**.jsonl.gz", split="train")
 
     spm.SentencePieceTrainer.train(
-        sentence_iterator=dataset_iterator(dataset),
+        sentence_iterator=dataset_iterator(dataset, args.batch_size),
         model_prefix=str(tokenizer_path.absolute()),
         vocab_size=args.vocab_size,
         model_type="bpe",
         max_sentence_length=4096,
         num_threads=args.num_threads,
-        unk_id=-1,
-        bos_id=0,
-        eos_id=1,
-        pad_id=2,
+        unk_id=0,
+        bos_id=1,
+        eos_id=2,
+        pad_id=3,
         byte_fallback=True,
         train_extremely_large_corpus=True
     )
