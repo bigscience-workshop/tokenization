0f5567b6d62e7c253d724f88390ba552a44a6419
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-02-06 08:26:22 +0100

    Try optimizing throughput

--- train_convert_tokenizer_simple.py
+++ train_convert_tokenizer_simple.py
@@ -1,8 +1,11 @@
 import logging
+from functools import partial
+from multiprocessing import Pool
 from pathlib import Path
+from typing import Callable
 
 import sentencepiece as spm
-from datasets import load_dataset, utils
+from datasets import load_dataset, utils, Dataset
 from datasets.utils.logging import set_verbosity_info
 from transformers.convert_slow_tokenizer import SpmConverter
 import argparse, os
@@ -16,43 +19,66 @@ def get_args():
     parser.add_argument("--data_name", "-d", type=str, required=True)
     parser.add_argument("--output_folder", "-o", type=Path, required=True)
     parser.add_argument("--num_threads", "-th", type=int, required=True)
-    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--load_batch_size", type=int, default=1)
     return parser.parse_args()
 
 def dataset_iterator(dataset, batch_size: int):
-    # WIP
-    # slices = [(start, min(len(datasets), start + batch_size)) for start in range(0, len(dataset), batch_size)]
-    # for i in utils.tqdm(
-    #     ,
-    #     total=len(dataset),
-    #     unit="ba",
-    #     disable=bool(utils.logging.get_verbosity() == utils.logging.NOTSET),
-    #     desc="Loading dataset",
-    # ):
-    #     # Load things by batch.
-    #     batch = dataset[i: i+batch_size]["text"]
-    #     for text in batch:
-    #         yield text
+    # # WIP
+    slices = [(start, min(len(dataset), start + batch_size)) for start in range(0, len(dataset), batch_size)]
+    for i in utils.tqdm(
+        slices,
+        total=len(dataset),
+        unit="ba",
+        disable=bool(utils.logging.get_verbosity() == utils.logging.NOTSET),
+        desc="Loading dataset",
+    ):
+        # Load things by batch.
+        batch = dataset[i: i+batch_size]["text"]
+        for text in batch:
+            # Removes None
+            if not text:
+                continue
+
+            text = text.strip()
+
+            # Remove all whitespaces
+            if not text:
+                continue
+
+            yield text
+
+    # for sample in dataset:
+    #     text = sample["text"]
     #
-    for sample in dataset:
-        text = sample["text"]
-
-        # Removes None
-        if not text:
-            continue
-
-        text = text.strip()
-
-        # Remove all whitespaces
-        if not text:
-            continue
-
-        yield text
+    #     # Removes None
+    #     if not text:
+    #         continue
+    #
+    #     text = text.strip()
+    #
+    #     # Remove all whitespaces
+    #     if not text:
+    #         continue
+    #
+    #     yield text
 
 
 class SPMTokenizer:
     def __init__(self, vocab_file):
         self.vocab_file = vocab_file
+
+def reduce_on_shard(index:int, num_shards: int, dataset: Dataset, batch_size: int, func: Callable[[int, str], int], init_acc: int):
+    shard = dataset.shard(num_shards=num_shards, index=index)
+    acc = init_acc
+    for text in dataset_iterator(shard, batch_size):
+        acc = func(acc, text)
+    return acc
+
+def get_max(acc, text):
+    length = len(text)
+    if length > acc:
+        return length
+    return acc
 
 def main():
     # Setup logging
@@ -70,11 +96,26 @@ def main():
     dataset = load_dataset(args.data_name, data_files="**.jsonl.gz", split="train")
 
     logger.info(f"Dataset length: {len(dataset)}")
-    max_length = 0
-    for text in dataset_iterator(dataset, args.batch_size):
-        length = len(text)
-        if max_length < length:
-            max_length = length
+    # max_length = 0
+    # for text in dataset_iterator(dataset, args.load_batch_size):
+    #     length = len(text)
+    #     if max_length < length:
+    #         max_length = length
+
+    # Parallel version
+    with Pool(args.num_threads) as pool:
+        max_per_shard = pool.map(
+            partial(
+                reduce_on_shard,
+                num_shards=args.num_threads,
+                dataset=dataset,
+                batch_size=args.batch_size,
+                func=get_max,
+                init_acc=0
+            ),
+            range(args.num_threads)
+        )
+        max_length=max(max_per_shard)
     logger.info(f"Max length: {max_length}")
 
     spm.SentencePieceTrainer.train(
