287fdc757887b608e3caabc099190eb46bc6776f
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-02-15 00:22:31 +0100

    Install tokenizers from source

--- setup_gcp.sh
+++ setup_gcp.sh
@@ -3,11 +3,28 @@ sudo apt-get install git -y
 sudo apt-get install wget -y
 sudo apth-get install tmux -y
 
+# Install Rust
+sudo apt install build-essential -y
+sudo apt-get install pkg-config
+sudo apt-get install libssl-dev
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
 # Setup conda
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
 sh Miniconda3-py38_4.10.3-Linux-x86_64.sh
 # You should enter a bunch of things manually
+
+# Clone tokenizers
+mkdir ~/code
+cd ~/code
+git clone https://github.com/huggingface/tokenizers.git
+cd tokenizers
+git checkout bigscience_fork
+cd bindings/python
+pip install setuptools_rust
+pip install -e .
 
 # Setup tokenization repo
 mkdir ~/code
